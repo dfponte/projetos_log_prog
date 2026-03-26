88cb526caef0d0886a597dbf8b81589f0f0d39c4
dfponte <danielpontep@hotmail.com>
2026-03-25 21:40:19 -0300

    projeto13_v2

--- projeto_13_for_exemplo2/main.c
+++ projeto_13_for_exemplo2/main.c
@@ -3,8 +3,18 @@
 #include <stdlib.h>
 
 int main(){
+     int y =0;
 
-   
+    // Projeto for equação 
+    for(int x=0;x<=100;x++){
+         y= x*x-25*x+100;
+         if(y==0){
+           printf("Para x = %d -> y = %d\n",x,y);
+         }
+
+    }
+
+    //Projeto com operadores de deslocamento e binário
     unsigned char regA = 0;
     regA = (1<<0);
     printf("Valor armazenado regA = %X\n",regA);
