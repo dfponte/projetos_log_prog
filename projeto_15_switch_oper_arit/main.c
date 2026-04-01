bf5060892e54bd2b5862c126dbeebe9b20c8f6b8
dfponte <danielpontep@hotmail.com>
2026-04-01 20:41:08 -0300

    proj_switch_car

---
+++ projeto_15_switch_oper_arit/main.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ 
+    char c;
+    char oper;
+    int valor1;
+    int valor2;
+    
+    printf("Digite o valor1 \n");
+    scanf("%d",&valor1);
+    scanf("%c",&c);
+    printf("Digite o valor2 \n");
+    scanf("%d",&valor2);
+    scanf("%c",&c);
+    printf("Digite operacao desejada +, - ou * \n");
+    scanf("%c",&oper);
+    scanf("%c",&c);
+   
+     switch(oper){
+
+
+        case '+':printf("Soma \n");
+        printf("Valor da soma = %d \n",valor1+valor2);
+        break;
+        case '-':printf("Subtracao\n");
+        printf("Valor da subtracao %d \n",valor1-valor2);
+        break;
+        case '*':printf("Multiplicacao\n");
+        printf("Valor do produto %d \n",valor1*valor2);
+        break;
+        default:printf("Valor inexitente");
+        }
+
+    
+
+    return 0;
+   
+}
