bed0031d0e85914f1b686897eb9e78c1af3be39a
dfponte <danielpontep@hotmail.com>
2026-04-01 21:16:32 -0300

    projeto_switch_char

--- projeto_15_switch_oper_arit/main.c
+++ projeto_15_switch_oper_arit/main.c
@@ -12,17 +12,18 @@ int main()
     printf("Digite o valor1 \n");
     scanf("%d",&valor1);
     scanf("%c",&c);
+
     printf("Digite o valor2 \n");
     scanf("%d",&valor2);
     scanf("%c",&c);
+
     printf("Digite operacao desejada +, - ou * \n");
     scanf("%c",&oper);
     scanf("%c",&c);
    
      switch(oper){
-
-
-        case '+':printf("Soma \n");
+        case '+':printf("Voce escolheu realizar uma soma\n");
+        printf("Entao vamos la... \n");
         printf("Valor da soma = %d \n",valor1+valor2);
         break;
         case '-':printf("Subtracao\n");
@@ -34,7 +35,26 @@ int main()
         default:printf("Oper inexitente");
         }
 
-    
+         int ch;
+         printf("Digite 1: comercial- digite 2: deposito - digite 3: atendente \n");
+         scanf("%d",&ch);
+         scanf("%c",&c);
+
+         switch (ch)
+         {
+         case 1: printf("Vc está no comercial");
+            /* code */
+            break;
+         case 2: printf("Vc está no deposito");
+            /* code */
+            break;
+         case 3: printf("Diga o que deseja...");
+            /* code */
+            break;
+         default:printf("Opcao invalida...");
+            break;
+         }
+
 
     return 0;
    
