694438834755431f2e801f24b19a7319ebd3ba12
dfponte <danielpontep@hotmail.com>
2026-05-04 19:49:37 -0300

    proj_17_func

--- projeto_17_funcao/main.c
+++ projeto_17_funcao/main.c
@@ -1,20 +1,36 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void meu_nome(void);
+void x(void);
+
+int novo_preco(int,int);
 
 int main(){
 
-   meu_nome();
+    x();
+
+    int preco_atual =0;
+    int desconto =0;
     
+    printf("Digite preco\n");
+    scanf("%d",&preco_atual);
+    
+    printf("Digite o desconto\n");
+    scanf("%d",&desconto);
+
+    int novo_p =  novo_preco(preco_atual,desconto);
+     printf("Novo preco e %d\n",novo_p);
     return 0;
 }
 
-void meu_nome(void){
+void x(void){
   
     for(int i=0;i<5;i++){
        printf("Daniel\n");
     }
+}p
+
+int novo_preco(int p,int d){
+   int n_preco = p - (p*d/100);
+   return n_preco;
 }
